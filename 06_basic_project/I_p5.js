4949baccfea0fb60d339c7c90163d997082f9ff4
Rahi20028 <rahipatel879@gmail.com>
2025-04-18 15:44:43 +0530

    task-06

--- 06_basic_project/I_p5.js
+++ 06_basic_project/I_p5.js
@@ -1,6 +1,20 @@
 
-let array = [2,6,1,4,8,3]
-let sorted_array =[]
+let array = [2,6,1,7,3,9]
+let sortedARR = []
 
+while (array.length>0){
+    let min = array[0]
+    let minIndex = 0
+    for(i=1; i<array.length; i++){
+        if(array[i]<min){
+            min= array[i]
+            minIndex = i
+        }
+}
+sortedARR.push(min)
 
-console.log(sorted_array);
+array.splice(minIndex,1)
+}
+console.log(array);
+
+console.log(sortedARR);
