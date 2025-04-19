65751eaa5e7d873433d7955ead14b3bd49cce5d3
Rahi20028 <rahipatel879@gmail.com>
2025-04-19 15:32:49 +0530

    task-06

--- 06_basic_project/I_p4.js
+++ 06_basic_project/I_p4.js
@@ -1,1 +1,21 @@
 // armstrong number check
+
+let num = 153
+const originalNum = num
+let StringNum = num.toString()
+let sum = 0
+while(num>0){
+    sum = sum + ((num%10)**StringNum.length)
+    num= Math.floor(num/10)
+  
+
+}
+console.log(sum);
+
+if(originalNum==sum){
+    console.log(" thee number is armstrong");
+    
+}else{
+    console.log("the number is not armstrong");
+    
+}
